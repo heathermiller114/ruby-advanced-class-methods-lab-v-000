e93241874f4dab76ad7805acf503f7cb98e6a67d
Heather Miller <heather.miller114@gmail.com>
2018-09-18 18:53:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -5,9 +7,37 @@ class Song
   def self.all
     @@all
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+  
   def save
     self.class.all << self
+  end
+  
+  def self.find_by_name(song_name)
+    song = self.new
+    song.name = song_name
+
+    @@all.detect{|song|song.name = name}
+    end
+    result
   end
 
 end
