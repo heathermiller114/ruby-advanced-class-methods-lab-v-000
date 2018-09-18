fec076fc2d033f841ce2674ef95f9d27119fb533
Heather Miller <heather.miller114@gmail.com>
2018-09-18 20:15:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
   def self.new_from_filename(mp3)
     song = self.new
     song.name = mp3.split(/-|.mp3/)[1]
-    song.artist_name = mp3.split('-')[0]
+    song.artist_name = mp3.split(/-|.mp3/)[0]
   end
 
 end
